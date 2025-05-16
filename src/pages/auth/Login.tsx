f128516dccbc70aeb433b4b8b696d7f9b07a9836
PRASUN-SITAULA <psitaula209@gmail.com>
2025-05-16 17:24:56 +0545

    Setup lint-staged, prettier, husky and ci pipeline

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,21 +1,21 @@
-import { LoginForm } from "@/components/auth/LoginForm";
-import { LockOpen } from "lucide-react";
+import { LoginForm } from '@/components/auth/LoginForm'
+import { LockOpen } from 'lucide-react'
 
 export const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="bg-white p-10 rounded-2xl shadow-2xl shadow-purple-400 w-full max-w-md border border-gray-200 animate-fade-in">
-        <div className="flex flex-col items-center mb-6">
-          <div className="w-16 h-16 mb-2 rounded-full flex bg-purple-200 items-center justify-center text-white font-bold text-lg">
+    <div className="flex min-h-screen items-center justify-center px-4">
+      <div className="animate-fade-in w-full max-w-md rounded-2xl border border-gray-200 bg-white p-10 shadow-2xl shadow-purple-400">
+        <div className="mb-6 flex flex-col items-center">
+          <div className="mb-2 flex h-16 w-16 items-center justify-center rounded-full bg-purple-200 text-lg font-bold text-white">
             <LockOpen className="text-black" />
           </div>
           <h2 className="text-3xl font-semibold text-gray-800">Welcome Back</h2>
-          <p className="text-gray-500 text-sm">
+          <p className="text-sm text-gray-500">
             Please sign in to your account
           </p>
         </div>
         <LoginForm />
       </div>
     </div>
-  );
-};
+  )
+}
